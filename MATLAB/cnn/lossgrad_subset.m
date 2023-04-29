25ebec1ac43a89b76723e6378aa9be2bb21c81f5
Eleven Liu <jiejyunliu@gmail.com>
2023-04-29 19:52:00 +0800

    log real implementation in pytorch BCELoss .h

--- MATLAB/cnn/lossgrad_subset.m
+++ MATLAB/cnn/lossgrad_subset.m
@@ -11,48 +11,25 @@ Y(prob.y_mapped(batch_idx) + model.nL*[0:num_data-1]') = 1;
 
 % fun
 K = size(Y, 1);
-net = feedforward(prob.data(:, batch_idx), model, net); % debug: 4.9579
+net = feedforward(prob.data(:, batch_idx), model, net);
 preds =  1./(1+exp(-net.Z{L+1}));  % sigmoid
-
-% pytorch
-% Our solution is that BCELoss clamps its log function outputs to be
-% greater than or equal to -100. This way, we can always have a finite loss value
-% and a linear backward method.
-% loss = dot(Y, max(log(preds), -100)) + dot(1-Y, max(log(1.-preds), -100));
-
 % this might be different
 loss = -(dot(Y, log(preds)) + dot(1-Y, log(1.-preds)));
-% size(loss)
 loss = sum(loss ./ K);
-% loss = mean(loss);
-% loss = norm(net.Z{L+1} - Y, 'fro')^2; % ??
-% dloss = 2*(net.Z{L+1} - Y)
 
-% loss = - (Y * log(sigmoid(x)) + (1-Y) * log(sigmoid(1-x)))
+% BCELoss with logits
+% max_val = (-net.Z{L+1}); % clamp min(0)
+% max_val(max_val < 0) = 0.;
+% loss = (1-Y) .* net.Z{L+1} + max_val + log((exp(-max_val) + exp(-net.Z{L+1}-max_val)));
 
-% sigmoid(x) = 1./(1+exp(-x)) = (1+exp(-x))^-1
-% sigmoid(x)' = exp(-x) / (1+exp(-x))^2
-%             = (1+exp(-x)-1) / (1+exp(-x))^2
-%             = (1+exp(-x)) / (1+exp(-x))^2 - 1 / (1+exp(-x))^2
-%             = 1 / (1+exp(-x)) - 1 / (1+exp(-x))^2
-%             = 1 / (1+exp(-x)) (1 - 1 / (1+exp(-x)))
-%             = sigmoid(x) * (1-sigmoid(x))
-
-% dloss = - ((Y / sigmoid(x)) * (sigmoid(x))' +
-%        	((1-Y) / sigmoid(1-x)) * (sigmoid(1-x))' * (1-x)')
-%       = - ((Y / sigmoid(x)) * (sigmoid(x) * (1-sigmoid(x))) +
-%        ((1-Y) / sigmoid(1-x)) * (sigmoid(1-x) * (1-sigmoid(1-x))) * (-1))
-%       = -Y * (1-sigmoid(x)) + (1-Y) * (1-sigmoid(1-x))
-
+% square error
+% loss = norm(net.Z{L+1} - Y, 'fro')^2;
 
 if strcmp(task, 'fungrad')
 	% grad
-    % TBD: how to calculate the backward pass of BCE?
-    % https://courses.grainger.illinois.edu/ECE417/fa2021/lectures/lec18.pdf
-
     % binary cross entropy
     sig =  exp(net.Z{L+1})/(1+exp(net.Z{L+1}));
-    v = (sig-Y); % .* net.Z{L+1};
+    v = (sig-Y);
 	% size(v)
     % v = (-Y) .* (1./net.Z{L+1}) + (1-Y) .* 1./(1-net.Z{L+1});
 	% v = 2*(net.Z{L+1} - Y); % batch size * label size, here v corresponds to dlossdZ{L+1}
